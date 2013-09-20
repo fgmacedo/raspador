b56d801af8103dd3b3f4382d1aeb5e9463db6236
Fernando Macedo <fgmacedo@gmail.com>
2013-09-20 00:26:04 -0300

    +classifiers on setup.py

--- setup.py
+++ setup.py
@@ -16,4 +16,17 @@ setup(
     url="http://github.org/fgmacedo/raspador",
     version='0.1.3',
     packages=['raspador'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
