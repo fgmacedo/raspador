4b9fddf4077d0493c16aa592465d870581bd8c4f
Fernando Macedo <fgmacedo@gmail.com>
2013-10-29 11:07:40 -0200

    thousand and decimal separators for FloatField, fixes #1

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -9,35 +9,35 @@ from raspador.fields import BaseField, StringField, FloatField, BRFloatField, \
 
 class TestBaseField(unittest.TestCase):
 
-    def test_should_retornar_valor_no_analizar(self):
+    def test_should_return_value_on_parse(self):
         s = "02/01/2013 10:21:51           COO:022734"
         field = BaseField(r'COO:(\d+)')
         value = field.parse_block(s)
         self.assertEqual(value, '022734')
 
-    def test_should_retornar_none_sem_search(self):
+    def test_should_return_none_without_search(self):
         s = "02/01/2013 10:21:51           COO:022734"
         field = BaseField()
         value = field.parse_block(s)
         self.assertEqual(value, None)
 
-    def test_should_aceitar_callback(self):
+    def test_should_accept_callback(self):
         s = "02/01/2013 10:21:51           COO:022734"
 
-        def dobro(value):
+        def double(value):
             return int(value) * 2
 
-        field = BaseField(r'COO:(\d+)', input_processor=dobro)
+        field = BaseField(r'COO:(\d+)', input_processor=double)
         value = field.parse_block(s)
         self.assertEqual(value, 45468)  # 45468 = 2 x 22734
 
-    def test_should_recusar_callback_invalido(self):
+    def test_should_refuse_invalid_callback(self):
         self.assertRaises(
             TypeError,
-            lambda: BaseField(r'COO:(\d+)', input_processor='pegadinha')
+            lambda: BaseField(r'COO:(\d+)', input_processor='not a callcabk')
         )
 
-    def test_should_utilizar_grupo_quando_informado(self):
+    def test_should_return_groups(self):
         s = "Contador de Reduções Z:                     1246"
         field = BaseField(r'Contador de Reduç(ão|ões) Z:\s*(\d+)', groups=1,
                           input_processor=int)
@@ -46,7 +46,7 @@ class TestBaseField(unittest.TestCase):
 
 
 class TestIntegerField(unittest.TestCase):
-    def test_should_obter_valor(self):
+    def test_should_return_value(self):
         s = "02/01/2013 10:21:51           COO:022734"
         field = IntegerField(r'COO:(\d+)')
         value = field.parse_block(s)
@@ -54,27 +54,41 @@ class TestIntegerField(unittest.TestCase):
 
 
 class TestFloatField(unittest.TestCase):
-    def test_should_obter_valor(self):
+    def test_should_return_value(self):
         s = "VENDA BRUTA DIÁRIA:                    793.00"
         field = FloatField(r'VENDA BRUTA DIÁRIA:\s+(\d+\.\d+)')
         value = field.parse_block(s)
         self.assertEqual(value, 793.0)
 
-    def test_should_obter_valor_com_separador_de_milhar(self):
+    def test_should_return_value_with_thousand_separator(self):
         s = "VENDA BRUTA DIÁRIA:                  10,036.70"
         field = FloatField(r'VENDA BRUTA DIÁRIA:\s+([\d,]+.\d+)')
         value = field.parse_block(s)
         self.assertEqual(value, 10036.7)
 
+    def test_should_return_value_with_custom_thousand_separator(self):
+        s = "VENDA BRUTA:                  10#036.70"
+        field = FloatField(r'VENDA BRUTA:\s+([\d#]+.\d+)',
+                           thousand_separator='#')
+        value = field.parse_block(s)
+        self.assertEqual(value, 10036.7)
+
+    def test_should_return_value_with_custom_decimal_separator(self):
+        s = "VENDA BRUTA:                  10,036#70"
+        field = FloatField(r'VENDA BRUTA:\s+([\d,]+#\d+)',
+                           decimal_separator='#')
+        value = field.parse_block(s)
+        self.assertEqual(value, 10036.7)
+
 
 class TestBRFloatField(unittest.TestCase):
-    def test_should_obter_valor(self):
+    def test_should_return_value(self):
         s = "VENDA BRUTA DIÁRIA:                    793,00"
         field = BRFloatField(r'VENDA BRUTA DIÁRIA:\s+(\d+,\d+)')
         value = field.parse_block(s)
         self.assertEqual(value, 793.0)
 
-    def test_should_obter_valor_com_separador_de_milhar(self):
+    def test_should_return_value_with_thousand_separator(self):
         s = "VENDA BRUTA DIÁRIA:                  10.036,70"
         field = BRFloatField(r'VENDA BRUTA DIÁRIA:\s+([\d.]+,\d+)')
         value = field.parse_block(s)
@@ -82,7 +96,7 @@ class TestBRFloatField(unittest.TestCase):
 
 
 class TestStringField(unittest.TestCase):
-    def test_should_obter_valor(self):
+    def test_should_return_value(self):
         s = "1   Dinheiro                                       0,00"
         field = StringField(r'\d+\s+(\w[^\d]+)')
         value = field.parse_block(s)
@@ -106,7 +120,7 @@ class TestBooleanField(unittest.TestCase):
 
 
 class TestDateField(unittest.TestCase):
-    def test_should_obter_valor(self):
+    def test_should_return_value(self):
         s = "02/01/2013 10:21:51           COO:022734"
         field = DateField(r'^(\d+/\d+/\d+)')
         value = field.parse_block(s)
@@ -122,7 +136,7 @@ class TestDateField(unittest.TestCase):
 
 
 class TestDateTimeField(unittest.TestCase):
-    def test_should_obter_valor(self):
+    def test_should_return_value(self):
         s = "02/01/2013 10:21:51           COO:022734"
         field = DateTimeField(r'^(\d+/\d+/\d+ \d+:\d+:\d+)')
         value = field.parse_block(s)
